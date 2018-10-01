bbd93701fe05a4df76d01b707cecc7a2c73f8098
lehuuthoct <lehuuthoct@gmail.com>
2018-10-01 13:57:39 +1000

    add styles for TodoList (List.css)

--- src/components/Todo/List.jsx
+++ src/components/Todo/List.jsx
@@ -1,28 +1,41 @@
 import React from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import './List.css';
 
 const List = props => (
   <ul>
-    {props.items.map((item, key) => (
-      <li key={key} className={` ${item.completed ? 'completed' : 'pending'} `}>
-        {item.task}
+    <ReactCSSTransitionGroup
+      transitionName="todo"
+      transitionAppear={true}
+      transitionEnterTimeout={500}
+      transitionLeaveTimeout={300}
+      transitionAppearTimeout={200}
+    >
+      {props.items.map((item, key) => (
+        <li
+          key={key}
+          className={` ${item.completed ? 'completed' : 'pending'} `}
+        >
+          {item.task}
 
-        {/* actions */}
-        <div className="actions">
-          {/* mark complete */}
-          <span
-            className={`${item.completed ? 'hide' : 'done'}`}
-            onClick={() => props.markAsCompleted(item.id)}
-          >
-            <i className="fa fa-check" />
-          </span>
+          {/* actions */}
+          <div className="actions">
+            {/* mark complete */}
+            <span
+              className={`${item.completed ? 'hide' : 'done'}`}
+              onClick={() => props.markAsCompleted(item.id)}
+            >
+              <i className="fa fa-check" />
+            </span>
 
-          {/* remove */}
-          <span className="trash" onClick={() => props.removeTask(item.id)}>
-            <i className="fa fa-trash" />
-          </span>
-        </div>
-      </li>
-    ))}
+            {/* remove */}
+            <span className="trash" onClick={() => props.removeTask(item.id)}>
+              <i className="fa fa-trash" />
+            </span>
+          </div>
+        </li>
+      ))}
+    </ReactCSSTransitionGroup>
   </ul>
 );
 
