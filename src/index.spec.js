875fc546c8d3fd8b3adc423a882b90dc3dd883d0
lehuuthoct <lehuuthoct@gmail.com>
2018-11-07 22:54:40 +1100

    add tests for equality by references and values

---
+++ src/index.spec.js
@@ -0,0 +1,29 @@
+describe('first test suite', function () {
+  it('should work', function () {
+    expect(true).toEqual(true);
+  });
+
+  it('should work again', function () {
+    expect(true).toEqual(true);
+  });
+
+});
+
+describe('equality test suite', function () {
+  it('should check for equal values', function () {
+    expect(5).toEqual(5);
+  });
+
+  it('should check for equal values using deep equality', function () {
+    // [] === [] : false
+    expect({ foo: 5}).toEqual({foo: 5});
+    expect([1,2,3]).toEqual([1,2,3]);
+    expect([1,2,3]).toContain(2);
+  });
+
+  it('should check that an array contains a specific object/array', function () {
+    // expect([{foo: 5}]).toContain({foo: 5}); // false - check by value
+    expect([{foo: 5}]).toContainEqual({foo: 5});
+  })
+});
+
